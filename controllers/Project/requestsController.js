49b00dfe441af6f1fc4925b0258bfd40af0f6f6b
Exorcist <mtalhead313@gmail.com>
2023-04-09 18:23:33 +0300

    Created 4 requests

--- controllers/Project/requestsController.js
+++ controllers/Project/requestsController.js
@@ -6,6 +6,7 @@ const WorkerCertificate = require('../../models/workerCertificate')
 const { Op } = require("sequelize");
 
 let id = 0
+let id2 = []
 
 exports.GetWorkersByCompanyName = async (req, res) => {
     if (!req.params.name){
@@ -77,5 +78,132 @@ exports.GetPositionsByWorker = async (req, res) => {
                 message: err.message || "Some error occurred while retrieving Workers by CompanyID"
             })
         })
+
+}
+
+exports.GetCompanyByWorker = async (req, res) => {
+    if (!req.params.name){
+        res.status(400).send({
+            message: 'Content cannot be empty!'
+        })
+        return
+    }
+
+    await Worker.findAll({
+        where:{
+            name: {[Op.like]: `%${req.params.name}%`}
+        }
+    })
+        .then(res => {
+            return res.map(row => {
+                id = row.dataValues.positionID
+                return id
+            })
+        })
+
+        Company.findAll({
+            where:{
+                company_id:{[Op.eq]: id},
+            }
+
+        })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving Workers by CompanyID"
+            })
+        })
+
+}
+
+exports.GetWorkerByPosition = async (req, res) => {
+    if (!req.params.name){
+        res.status(400).send({
+            message: 'Content cannot be empty!'
+        })
+        return
+    }
+
+    await Position.findAll({
+        where:{
+            name: {[Op.like]: `%${req.params.name}%`}
+        }
+    })
+        .then(res => {
+            return res.map(row => {
+                id = row.dataValues.position_id
+                return id
+            })
+        })
+
+    Worker.findAll({
+        where:{
+            positionID:{[Op.eq]: id},
+        }
+
+    })
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving Workers by CompanyID"
+        })
+    })
+
+}
+
+exports.GetCertificatesByWorker = async (req, res) => {
+    if (!req.params.name){
+        res.status(400).send({
+            message: 'Content cannot be empty!'
+        })
+        return
+    }
+
+    await Worker.findAll({
+        where:{
+            name: {[Op.like]: `%${req.params.name}%`}
+        }
+    })
+        .then(res => {
+            return res.map(row => {
+                id = row.dataValues.worker_id
+                return id
+            })
+        })
+
+    WorkerCertificate.findAll({
+        where:{
+            workerId:{[Op.eq]: id},
+        }
+
+    })
+        .then(res => {
+            return res.map(row => {
+                id2 = row.dataValues.certificateId;
+                console.log(row.dataValues);
+                return id2
+            })
+        })
+        console.log(id2.length)
+    for (let i = 0; i < id2.length; i++) { 
+        Certificate.findAll({
+            where:{
+                certificate_id: id2[i]
+                
+            }
+        })
+            .then(data => {
+                res.send(data);
+            })
+            .catch(err => {
+                res.status(500).send({
+                        message: err.message || "Some error occurred while retrieving Workers by CompanyID"
+                    })
+                })
+    }
 
 }
--- routes/Project/requestsRoutes.js
+++ routes/Project/requestsRoutes.js
@@ -50,5 +50,77 @@ module.exports = app => {
     //Get Worker By CompanyID
     router.get('/PositionByWorkerName/:name', request.GetPositionsByWorker)
 
-    app.use('/api/request', router)
+    /**
+     * 
+     * @swagger
+     * /api/request/CompanyByWorkerName/{name}:
+     *   get:
+     *     summary: GET Position by WorkerName
+     *     parameters:
+     *       - in: path
+     *         name: name
+     *         schema:
+     *           type: string
+     *         required: true
+     *         description: Name of the worker to get company
+     *     responses:
+     *       '200':
+     *         description: OK
+     *       '400':
+     *         description: INVALID NAME GIVEN
+     *       '404':
+     *         description: NAME NOT FOUND
+     */
+    //Get Worker By CompanyID
+    router.get('/CompanyByWorkerName/:name', request.GetCompanyByWorker)
+
+    /**
+     * 
+     * @swagger
+     * /api/request/WorkerByPositionName/{name}:
+     *   get:
+     *     summary: GET Worker by PositionName
+     *     parameters:
+     *       - in: path
+     *         name: name
+     *         schema:
+     *           type: string
+     *         required: true
+     *         description: Name of the position to get worker
+     *     responses:
+     *       '200':
+     *         description: OK
+     *       '400':
+     *         description: INVALID NAME GIVEN
+     *       '404':
+     *         description: NAME NOT FOUND
+     */
+    //Get Worker By CompanyID
+    router.get('/WorkerByPositionName/:name', request.GetWorkerByPosition)
+
+    /**
+     * 
+     * @swagger
+     * /api/request/CertificateByWorkerName/{name}:
+     *   get:
+     *     summary: GET Certificate by WorkerName
+     *     parameters:
+     *       - in: path
+     *         name: name
+     *         schema:
+     *           type: string
+     *         required: true
+     *         description: Name of the Worker to get Certificate
+     *     responses:
+     *       '200':
+     *         description: OK
+     *       '400':
+     *         description: INVALID NAME GIVEN
+     *       '404':
+     *         description: NAME NOT FOUND
+     */
+    //Get Worker By CompanyID
+    router.get('/CertificateByWorkerName/:name', request.GetCertificatesByWorker)
+
+    app.use('/api/request', router) 
 }
